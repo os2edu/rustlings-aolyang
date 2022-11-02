986ce031a3c1abdffbf0b7935cf32d45dbaccabf
aolyang <aolyang@foxmail.com>
2022-11-02 23:18:51 +0800

    finish string

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,32 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    // input.trim().to_string()
+    let mut result = String::new();
+    let mut wp = String::new();
+    for s in input.chars() {
+        if s == ' ' {
+            wp.push(s)
+        } else {
+            if result.len() != 0 { result += &wp }
+
+            wp.clear();
+            result.push(s)
+        }
+    }
+    result
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
