7aec65ec07c7401795bd6be185dc1a10b235f3ce
aolyang <aolyang@foxmail.com>
2022-11-14 19:56:34 +0800

    some iterators

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -13,6 +11,14 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let mut result: u64 = 1;
+    if num > 1 {
+        for n in 1..=num {
+            println!("number {}", n);
+            result *= n
+        } 
+    }
+    result
 }
 
 #[cfg(test)]
