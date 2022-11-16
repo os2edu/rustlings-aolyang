7293c47bbad0ea27f8ac3d074b0ff1429e449dc5
aolyang <aolyang@foxmail.com>
2022-11-16 11:18:26 +0800

    finish clippy

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,13 +1,13 @@
 // clippy2.rs
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
-        res += x;
+    if let Some(n) = option {
+        for x in 0..n {
+            res += x;
+        }
     }
     println!("{}", res);
 }
